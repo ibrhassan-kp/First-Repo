ee385c51dcfe8a97bbe79af862608e2a91b70d16
K246931 <ibrahim.hassan@kp.org>
2021-01-13 15:08:09 -0800

    added new system printout

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git ");
+        System.out.println("this is the second change to the code");
     }
 
 }
